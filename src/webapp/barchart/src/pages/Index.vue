f6c7a336b8120a36cae24e07b345a7b25a7b8842
Christine Meranda <christine@hindsightlabs.com>
2020-05-09 20:52:18 -1000

    Show legend, show national prices, format to show currency.

--- src/webapp/barchart/src/pages/Index.vue
+++ src/webapp/barchart/src/pages/Index.vue
@@ -13,20 +13,22 @@
     <q-card class="my-card">
       <q-card-section>
         <div class="text-h6 text-grey-8 text-weight-bolder">
-          Hawaii Avocado Prices (each)
-        </div>
-        <div class="text-subtitle1 text-grey-8">
-          Average avocado weight: <b>0.4 lbs</b>
+          Avocado Prices Each
         </div>
       </q-card-section>
       <q-card-section class="q-pa-none echarts">
         <IEcharts :option="eachLineChartOption" :resizable="true" />
       </q-card-section>
+      <q-card-section>
+        <div class="text-subtitle1 text-grey-8">
+          Average avocado weight: <b>0.4 lbs</b>
+        </div>
+      </q-card-section>
     </q-card>
     <q-card class="my-card">
       <q-card-section>
         <div class="text-h6 text-grey-8 text-weight-bolder">
-          Hawaii Avocado Prices (pound)
+          Avocado Prices by Pound
         </div>
       </q-card-section>
       <q-card-section class="q-pa-none echarts">
@@ -43,11 +45,18 @@ import json from '../json/hawaii_avocado_prices.json'
 const eachPrices = json.each_prices
 const poundPrices = json.pound_prices
 
+import natlJson from '../json/national_avocado_prices.json'
+const natlEachPrices = natlJson.each_prices
+const natlPoundPrices = natlJson.pound_prices
+
+const numeral = require('numeral')
+
+// How to make/style charts: https://echarts.apache.org/en/tutorial.html#ECharts%20Basic%20Concepts%20Overview
+
 export default {
   name: 'charts',
   data () {
     return {
-      eachPricesJson: eachPrices,
       barChartOption: {
         grid: {
           bottom: '25%'
@@ -80,41 +89,75 @@ export default {
       },
       eachLineChartOption: {
         grid: {
-          bottom: '25%'
+          left: '15%',
+          bottom: '5%'
         },
-        legend: {},
+        legend: {
+          data: ['Hawaii', 'US national']
+        },
         tooltip: {},
         xAxis: {
           type: 'category',
           data: eachPrices.map(d => d.month_year)
         },
-        yAxis: {},
+        yAxis: {
+          axisLabel: {
+            formatter: function (value) {
+              return numeral(value).format('$0.00').toUpperCase()
+            }
+          }
+        },
         // Declare several bar series, each will be mapped
         // to a column of dataset.source by default.
         series: [
           {
+            name: 'Hawaii',
             type: 'line',
-            data: eachPrices.map(d => d.avg_price)
+            data: eachPrices.map(d => d.avg_price),
+            smooth: true,
+            itemStyle: { color: '#ff851c' }
+          },
+          {
+            name: 'US national',
+            type: 'line',
+            data: natlEachPrices.map(d => d.avg_price),
+            smooth: true,
+            itemStyle: { color: '#40ecff' }
           }
         ]
       },
       poundLineChartOption: {
         grid: {
+          left: '15%',
           bottom: '25%'
         },
-        legend: {},
+        legend: {
+          data: ['Hawaii', 'US national']
+        },
         tooltip: {},
         xAxis: {
           type: 'category',
           data: poundPrices.map(d => d.month_year)
         },
-        yAxis: {},
+        yAxis: {
+          axisLabel: {
+            formatter: function (value) {
+              return numeral(value).format('$0.00').toUpperCase()
+            }
+          }
+        },
         // Declare several bar series, each will be mapped
         // to a column of dataset.source by default.
         series: [
           {
+            name: 'Hawaii',
             type: 'line',
             data: poundPrices.map(d => d.avg_price)
+          },
+          {
+            name: 'US national',
+            type: 'line',
+            data: natlPoundPrices.map(d => d.avg_price)
           }
         ]
       }
